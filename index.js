8c0d856197780391fed7f1f8dd9f869f0588805b
shahrukh-hughes <s.khan_1987@hotmail.co.uk>
2021-11-10 16:12:19 +0000

    back-end-development

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 import express from 'express'
 
 const app = express()
-
 const port = 4005
+
+console.log('UUID', uuid())
+
+app.use((req, _res, next) => {
+  console.log(`Request recieved: ${req.method} - ${req.url}`)
+  next()
+})
+
+app.use((_req, res) => {
+  res.end(`Route not found`)
+})
 
 app.listen(port, () => {
   console.log(`Server up and running on port ${port}`)
