01b06c625f128cd6e49041bdf1c3fcd9f5fa6f6a
shahrukh-hughes <s.khan_1987@hotmail.co.uk>
2021-11-10 22:54:02 +0000

    added seeded batabase

---
+++ db/data/movies.js
@@ -0,0 +1,11 @@
+export default [
+  {
+    title: 'Snatch',
+    director: 'Guy Ritchie',
+    releaseYear: 2001,
+    description: 'Brad Pitt is a bare-knuckle boxer',
+    genre: 'action comedy',
+    cast: ['Brad Pitt', 'Jason Stathom', 'Vinnie Jones'],
+    rating: 9
+}
+]
--- db/seeds.js
+++ db/seeds.js
@@ -1,20 +1,28 @@
 import mongoose from 'mongoose'
 import { dbURI } from '../config/environment.js'
+import Movie from '../models/movie.js'
+import movieData from './data/movies.js'
 
 const seedDatabase = async () => {
   try {
     //Connect to the db
     await mongoose.connect(dbURI)
-    console.log(`Database connected`)
+    console.log(`DB connected`)
     //Drop the db
-
+    await mongoose.connection.db.dropDatabase()
+    console.log(`DB Dropped`)
     //Seed the db with data file
-
+    const moviesAdded = Movie.create(movieData)
+    console.log(`${moviesAdded.length} Movies added`)
     //Close our connection to db
-
-    //Close the connection to db
+    await mongoose.connection.close()
+    console.log(`Bye`)
+    
   } catch (err) {
     console.log(err)
+    //Close the connection to db
+    await mongoose.connection.close()
+    console.log(`Error. DB connection closed`)
   }
 }
 
