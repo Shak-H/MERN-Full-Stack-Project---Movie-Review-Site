e23dcef6ac770f66c7a2c446e4b683c74a536fe8
shahrukh-hughes <s.khan_1987@hotmail.co.uk>
2021-11-11 00:41:11 +0000

    added register and log in functions

--- config/router.js
+++ config/router.js
@@ -2,7 +2,7 @@ import express from 'express'
 
 //Bringing in our controllers
 import { addMovie, getAllMovies, getSingleMovie, removeMovie, updateMovie } from '../controllers/movies.js'
-import { registerUser } from '../controllers/auth.js'
+import { loginUser, registerUser } from '../controllers/auth.js'
 
 //Invoking Express router
 const router = express.Router()
@@ -13,6 +13,8 @@ router.route('/movies').get(getAllMovies).post(addMovie)
 router.route('/movies/:id').get(getSingleMovie).put(updateMovie).delete(removeMovie)
 
 router.route('/register').post(registerUser)
+
+router.route('/login').post(loginUser)
 
 //Exporting the router to be used as middleware in index.js
 export default router 
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,36 @@
 import User from '../models/users.js'
+import jwt from 'jsonwebtoken'
+import { secret } from '../config/environment.js'
 
 //Post /register
-//register a user
+//registers a user
 export const registerUser = async (req, res) => {
   try {
-    const newUser = User.create(req.body)
+    const newUser = await User.create(req.body)
     console.log(newUser)
     return res.status(202).json({ message: `Welcome ${newUser.username}` })
   } catch (err) {
     console.log(err)
     return res.status(422).json(err)
+  }
+}
+
+//Post /login
+//logs in a user
+export const loginUser = async (req, res) => {
+  try {
+    const userToLogin = await User.findOne({ username: req.body.username })
+    console.log('USER ->', userToLogin)
+    //Checks if password matches hashed password
+    if (!userToLogin || !userToLogin.validatePassword(req.body.password)){
+      throw new Error()
+    }
+    //Sends token to user
+    const token = jwt.sign({ sub: userToLogin._id }, secret, { expiresIn: '7 days'})
+    
+    return res.status(200).json({ message: `Welcome back ${userToLogin.username}`, token: token})
+  } catch (err) {
+    console.log(err)
+    return res.status(422).json({ message: 'Unauthorised' })    
   }
 }
