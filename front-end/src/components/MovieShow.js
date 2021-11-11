6d76452a12d9d2842e01f386beb9b87fccb499a8
eddie ruxton <ae.ruxton@gmail.com>
2021-11-11 21:04:11 +0000

    bootstrap css included, single movies page updated

--- front-end/src/components/MovieShow.js
+++ front-end/src/components/MovieShow.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-import MovieCard from './MovieCard'
 import { useParams } from 'react-router-dom'
+import Form from 'react-bootstrap/Form'
 
 const MovieShow = () => {
   const [movie, setMovie] = useState([])
+  const [genre, setGenre] = useState([])
   const { id } = useParams()
+  
 
   useEffect(() => {
     async function fetchMovie() {
@@ -19,17 +21,95 @@ const MovieShow = () => {
   
       const response = await axios(config)
       console.log(response.data)
+      setGenre(response.data.genre)
       setMovie(response.data)
     }
     fetchMovie()
   }, [id])
-    
+
   return (
     <div>
-      <h1>
-        {movie.title}
-      </h1>
-      <MovieCard {...movie} />
+      <img src={movie.image}/>
+      <div>
+        <h1>
+          {movie.title}
+        </h1>
+        <div className="movie-info-div">
+          <p>Director: {movie.director}</p>
+          <p>Released: {movie.releaseYear}</p>
+          <p>Description: {movie.description}</p>
+          <p>Genre: {genre.join(', ')}</p>
+          <p>Rating: {movie.averageRating}</p>
+        </div>
+        <div className="rate-n-review-div">
+          <Form>
+            {['radio'].map((type) => (
+              <div key={`inline-${type}`} className="mb-3">
+                <Form.Check
+                  inline
+                  label="1"
+                  name="group1"
+                  type={type}
+                />
+                <Form.Check
+                  inline
+                  label="2"
+                  name="group1"
+                  type={type}
+                />
+                <Form.Check
+                  inline
+                  label="3"
+                  name="group1"
+                  type={type}
+                />
+                <Form.Check
+                  inline
+                  label="4"
+                  name="group1"
+                  type={type}
+                />
+                <Form.Check
+                  inline
+                  label="5"
+                  name="group1"
+                  type={type}
+                />
+                <Form.Check
+                  inline
+                  label="6"
+                  name="group1"
+                  type={type}
+                />
+                <Form.Check
+                  inline
+                  label="7"
+                  name="group1"
+                  type={type}
+                />
+                <Form.Check
+                  inline
+                  label="8"
+                  name="group1"
+                  type={type}
+                />
+                <Form.Check
+                  inline
+                  label="9"
+                  name="group1"
+                  type={type}
+                />
+                <Form.Check
+                  inline
+                  label="10"
+                  name="group1"
+                  type={type}
+                />
+              </div>
+            ))}
+          </Form>
+        </div>
+      </div>
     </div>
   )
 }
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
-import './styles/main.css'
+import './styles/main.scss'
 import App from './App'
 
 ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'))
