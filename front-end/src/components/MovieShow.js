fefeea7edc95d056c7fbcf9c03805c54d447ab7e
eddie ruxton <ae.ruxton@gmail.com>
2021-11-12 11:12:57 +0000

    add a comment box included in rating form - image sizing adjusted

--- front-end/src/components/MovieShow.js
+++ front-end/src/components/MovieShow.js
@@ -7,6 +7,7 @@ import RatingForm from './RatingForm'
 const MovieShow = () => {
   const [movie, setMovie] = useState([])
   const [genre, setGenre] = useState([])
+  const [comments, setComments] = useState([])
   const { id } = useParams()
   
 
@@ -22,6 +23,7 @@ const MovieShow = () => {
       const response = await axios(config)
       console.log(response.data)
       setGenre(response.data.genre)
+      setComments(response.data.rating)
       setMovie(response.data)
     }
     fetchMovie()
@@ -29,7 +31,7 @@ const MovieShow = () => {
 
   return (
     <div>
-      <img src={movie.image}/>
+      <img src={movie.image} alt={movie.title} style={{ maxWidth: '500px', maxHeight: '600px' }}/>
       <div>
         <h1>
           {movie.title}
@@ -43,6 +45,15 @@ const MovieShow = () => {
         </div>
         <div className="rate-n-review-div">
           <RatingForm />
+        </div>
+        <div>
+          <ul>
+            {comments.map((comment) => (
+              <li key={comment._id}>
+                {comment.text}
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </div>
